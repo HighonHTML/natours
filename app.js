a34f4559369dc12550ee61650b9b926768ceea7b
HighonHTML <125417055+HighonHTML@users.noreply.github.com>
2024-06-12 17:59:23 +0530

    fix error: users route

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
 // mounting a new router on a routes
 
 app.use("/api/v1/tours", tourRouter);
-app.use("/api/v1/user", userRouter);
+app.use("/api/v1/users", userRouter);
 
 // server
 
