d95cb88f2fb802797ec28ab0c369658c40d1349b
HighonHTML <125417055+HighonHTML@users.noreply.github.com>
2024-06-12 17:58:40 +0530

    import controllers

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,61 +1,10 @@
 const express = require("express");
-const fs = require("fs");
+const userController = require("./../controllers/userController");
 
-const users = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/users.json`, "utf-8")
-);
+const { getAllUsers, createUser, getUser, updateUser, deleteUser } =
+  userController;
 
-const createUser = (req, res) => {
-  // const newId = parseFloat(users[users.length - 1]._id.slice(-1)) + 1;
-  // const newUser = Object.assign({ _id: newId }, req.body);
-  // users.push(newUser);
-  // fs.writeFile(
-  //   `${__dirname}/dev-data/data/users.json`,
-  //   JSON.stringify(users),
-  //   (err) => {
-  //     res.status(201).json({
-  //       status: "success",
-  //       data: {
-  //         user: newUser,
-  //       },
-  //     });
-  //   }
-  // );
-  res.status(500).json({
-    status: "error",
-    message: "this route is not defined yet",
-  });
-};
-
-const getUser = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "this route is not defined yet",
-  });
-};
-
-const getAllUsers = (req, res) => {
-  res.status(200).json({
-    status: "success",
-    data: {
-      users,
-    },
-  });
-};
-
-const updateUser = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "this route is not defined yet",
-  });
-};
-
-const deleteUser = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "this route is not defined yet",
-  });
-};
+// console.log(getAllUsers)
 
 const router = express.Router();
 
