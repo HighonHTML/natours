c456f52fbd3f187bf8bc92cb23848b31140ddbad
HighonHTML <125417055+HighonHTML@users.noreply.github.com>
2024-06-14 03:52:14 +0530

    import env variables

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-const app = require('./app')
+const dotenv = require("dotenv");
+dotenv.config({ path: `${__dirname}/config.env` });
+const app = require("./app");
 
-const port = 8000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`app running on port ${port}`);
