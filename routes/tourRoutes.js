c22522294d9750b24202ea5c6cd6579133c1db13
HighonHTML <125417055+HighonHTML@users.noreply.github.com>
2024-06-13 17:08:22 +0530

    mount the checkbody middleware

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const tourController = require("./../controllers/tourController");
 
-const { checkId, getAllTours, createTour, getTour, updateTour, deleteTour } =
+const { checkBody, checkId, getAllTours, createTour, getTour, updateTour, deleteTour } =
   tourController;
 
 const router = express.Router();
 
 router.param("id", checkId);
 
-router.route("/").get(getAllTours).post(createTour);
+router.route("/").get(getAllTours).post(checkBody, createTour);
 router.route("/:id").get(getTour).patch(updateTour).delete(deleteTour);
 
 module.exports = router;
